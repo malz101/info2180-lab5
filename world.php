b3cb409a1148cc01d528ea26d18a68559ee03814
Malik Edwards <malikedwards777@gmail.com>
2020-11-22 18:24:33 -0500

    Exercise 4 Completed - Display Additional Countries Information in a Table

--- world.php
+++ world.php
@@ -10,9 +10,9 @@ try{
   $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
   if($_SERVER['REQUEST_METHOD'] === 'GET'){
     if(isset($_GET['country']) and !preg_match("/[^( ,\.\-\(\)\w)]/", $_GET['country'])){
-      $country=$_GET['country'];
-      $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
-      $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+      $query=$_GET['country'];
+      $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$query%'");
+      $contries = $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
     else{
       echo "Invalid query string";
@@ -24,8 +24,25 @@ try{
 }
 ?>
 
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+<table>
+  <caption>List of Contries</caption>
+  <thead>
+      <tr>
+          <th>Name</th>
+          <th>Continent</th>
+          <th>Independence Year</th>
+          <th>Head of State</th>
+      </tr>
+  </thead>
+  <tbody>
+      <?php foreach ($contries as $country): ?>
+      <tr>
+          <td><?= $country['name']; ?></td>
+          <td><?= $country['continent']; ?></td>
+          <td><?= $country['independence_year']; ?></td>
+          <td><?= $country['head_of_state']; ?></td>
+      </tr>
+      <?php endforeach; ?>
+  </tbody>
+</table>
+      
